fbd40fd67cab344dc6c1c72df915b398c7d0d825
杨子寅 <86057935+PercivalYang@users.noreply.github.com>
2023-05-11 15:52:13 +0800

    添加拦截器用于确认用户已登陆

--- src/main/java/com/hmdp/config/LoginInterceptor.java
+++ src/main/java/com/hmdp/config/LoginInterceptor.java
@@ -1,2 +1,38 @@
-package com.hmdp.config;public class LoginInterceptor {
+package com.hmdp.config;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import com.hmdp.utils.UserHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    // 登陆前验证用户是否存在
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 从请求中获取会话
+        HttpSession session = request.getSession();
+        // 获取用户属性
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            // 用户不存在则进行拦截，返回401未授权错误码
+            log.info("用户不存在");
+            response.setStatus(401);
+            return false;
+        }
+        UserHolder.saveUser(user);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 在请求结束后清除用户信息，避免内存泄漏
+        UserHolder.removeUser();
+    }
 }
--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,2 +1,26 @@
-package com.hmdp.config;public class MvcConfig {
+package com.hmdp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                );
+    }
 }
